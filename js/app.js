d7a77c46b6cca1a712c154eddcf1698e7ebc69a8
Bogs Paulino <bogs.paulino@gmail.com>
2021-03-02 22:14:08 +0800

    added gallery lightbox

--- js/app.js
+++ js/app.js
@@ -1,5 +1,23 @@
+// Animations
+const fadeIn = (element, visibility = 1) => {
+    element.style.display = 'block';
+    gsap.fromTo(element, { opacity: 0 }, { opacity: visibility, duration: .3 });
+}
+
+const fadeOut = (element, visibility = 1) => {
+    gsap.fromTo(element, { opacity: visibility }, {
+        opacity: 0,
+        duration: .3,
+        onComplete: () => {
+            element.style.display = 'none';
+        }
+    });
+}
+
+// Mobile Menu
 const header = document.querySelector('header');
 header.classList.add('closed');
+
 const hamburger = document.querySelector('.hamburger');
 
 const openMobileNav = gsap.timeline({ paused: true });
@@ -30,3 +48,30 @@ function screenReady() {
 }
 
 screenReady();
+
+// Lightbox
+var elem = document.createElement("img");
+const lightbox = document.querySelector('.lightbox');
+lightbox.addEventListener('click', () => {
+    fadeOut(lightbox);
+});
+const galleryLinks = [];
+
+const showLightbox = (src) => {
+    fadeIn(lightbox);
+    lightbox.appendChild(elem);
+    elem.src = src;
+}
+
+const getGalleryItems = () => {
+    document.querySelectorAll('.gallery-img img').forEach(element => {
+
+        galleryLinks.push(element.src);
+
+        element.addEventListener('click', () => {
+            showLightbox(element.src);
+        })
+    });
+}
+
+getGalleryItems();
