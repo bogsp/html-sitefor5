c475ae20667ed5666aaed4eac4fef594a2df01d5
Bogs Paulino <bogs.paulino@gmail.com>
2021-03-02 17:42:53 +0800

    fixed navigation

--- js/app.js
+++ js/app.js
@@ -2,17 +2,19 @@ const header = document.querySelector('header');
 header.classList.add('closed');
 const hamburger = document.querySelector('.hamburger');
 
-const openMobileNav = gsap.timeline({ paused: true })
-    .from('.overlay', .5, { x: 250, opacity: 0 })
-    .from('.nav-links a', .5, { x: 100, opacity: 0, stagger: .1 }, "-= .2");
+const openMobileNav = gsap.timeline({ paused: true });
 
-const closeMobileNav = gsap.timeline({ paused: true })
-    .to('.overlay', .8, { x: 250, opacity: 0 })
-    .to('.nav-links a', .3, { x: 100, stagger: .1 }, "-= .2")
-    .to('.nav-links a', .2, { opacity: 0 }, "-= .5");
+const closeMobileNav = gsap.timeline({ paused: true });
 
 function screenReady() {
     if (screen.width <= 425) {
+        openMobileNav
+            .from('.overlay', .5, { x: 250, opacity: 0 })
+            .from('.nav-links a', .5, { x: 100, opacity: 0, stagger: .1 }, "-= .2");
+        closeMobileNav
+            .to('.overlay', .8, { x: 250, opacity: 0 })
+            .to('.nav-links a', .3, { x: 100, stagger: .1 }, "-= .2")
+            .to('.nav-links a', .2, { opacity: 0 }, "-= .5");
         hamburger.addEventListener('click', () => {
             if (header.classList.contains('closed') && !header.classList.contains('open')) {
                 openMobileNav.restart();
